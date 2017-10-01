0c1f0a088d05cbff138cdc9c367e06b33b1f1bc1
jace <jacewalker@me.com>
2017-10-01 18:53:43 +1100

    created UIInterface

---
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,30 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Jace Walker on 1/10/17.
+//  Copyright © 2017 Jace Walker. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+        
+        
+    }
+
+}
